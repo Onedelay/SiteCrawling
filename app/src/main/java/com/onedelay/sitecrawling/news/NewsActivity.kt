257a537b85f3b4132523ca715288af8f29a1e4f2
Onedelay <wjy0218@gmail.com>
2018-10-31 23:32:24 +0900

    SharedPreference 변경 리스너 구현

--- app/src/main/java/com/onedelay/sitecrawling/news/NewsActivity.kt
+++ app/src/main/java/com/onedelay/sitecrawling/news/NewsActivity.kt
@@ -21,7 +21,7 @@ import kotlinx.android.synthetic.main.fragment_news.view.*
 import org.jsoup.Jsoup
 import java.io.IOException
 
-class NewsActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     private lateinit var sharedPref: SharedPreferences
@@ -47,7 +47,6 @@ class NewsActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
-
         container.adapter = mSectionsPagerAdapter
 
         for (category in categories) {
@@ -58,6 +57,24 @@ class NewsActivity : AppCompatActivity() {
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
     }
 
+    // SharedPreference 내용이 변경될 때마다 호출되는 리스너
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        categories = when (p0?.getString(p1, Constants.NAVER)) {
+            Constants.DAUM -> daumCategories
+            else -> naverCategories
+        }
+
+        // 탭은 제거하지만 뷰페이저 내용은 안지워지는 듯 함
+        tabs.removeAllTabs()
+        mSectionsPagerAdapter!!.notifyDataSetChanged() // 모든탭 제거 후 notify 해주지 않으면 오류 발생
+
+        for (category in categories) {
+            tabs.addTab(tabs.newTab().setText(category))
+        }
+
+        mSectionsPagerAdapter!!.notifyDataSetChanged() // 새로 탭을 추가했기 때문에 호출해주어야 함
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_news, menu)
         return true
@@ -66,16 +83,28 @@ class NewsActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
+        // 어떤 portal 의 뉴스를 보여줄 지 옵션메뉴에서 설정
         portal = when (id) {
             R.id.daum -> Constants.DAUM
             else -> Constants.NAVER
         }
 
+        // shared preference 내용 변경
         with(sharedPref.edit()) {
             putString(Constants.SELECTED_PORTAL, portal)
             apply()
         }
         return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sharedPref.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPref.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
@@ -86,6 +115,7 @@ class NewsActivity : AppCompatActivity() {
 
     class PlaceholderFragment : Fragment(), NewsListAdapter.OnNewsClickListener, NaverNewsAsyncTask.OnTaskComplete {
         private val adapter = NewsListAdapter(this)
+
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                                   savedInstanceState: Bundle?): View? {
             val rootView = inflater.inflate(R.layout.fragment_news, container, false)
@@ -94,10 +124,16 @@ class NewsActivity : AppCompatActivity() {
             rootView.recyclerView.layoutManager = LinearLayoutManager(context)
             rootView.recyclerView.adapter = adapter
 
+            return rootView
+        }
+
+        /* 다음 -> 네이버로 변경시 뷰페이저 0~2 번째 내용이 비어있음 (다음 크롤링 구현이 안되어있기 때문에)
+         * 반대의 경우에는 네이버의 내용이 그대로 보여짐. (프래그먼트에 정보가 유지되는 것 같음. 페이저 어댑터의 내용은 지울 수 없을까?)
+         * 그래서 탭을 옮길 때 마다 새롭게 크롤링을 하도록 구현. 그러나 당연한 버벅거림이 발생 */
+        override fun onResume() {
+            super.onResume()
             val task = NaverNewsAsyncTask(arguments!!.getString(CATEGORY), this)
             task.execute()
-
-            return rootView
         }
 
         companion object {
@@ -133,7 +169,7 @@ class NaverNewsAsyncTask(private val category: String, private val listener: OnT
         try {
             val doc = Jsoup.connect("https://news.naver.com/").get()
             val elements = doc.select("ul.section_list_ranking")
-            for ((i, element) in elements.withIndex()) {
+            for (element in elements) {
                 if (element.parents()[0].select("h5").text() == category) {
                     for ((j, child) in element.children().withIndex()) {
                         val title = child.select("a").attr("title")
