13cbdf714f14107491292e98bfa6ae87db7c7bda
Onedelay <wjy0218@gmail.com>
2018-11-12 20:41:05 +0900

    progress bar null 처리

--- app/src/main/java/com/onedelay/sitecrawling/news/PlaceholderFragment.kt
+++ app/src/main/java/com/onedelay/sitecrawling/news/PlaceholderFragment.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -60,6 +61,6 @@ class PlaceholderFragment : Fragment(), NewsListAdapter.OnNewsClickListener, New
     override fun onTaskComplete(list: List<NewsItem>?) {
         adapter.setItems(list!!)
         rootView.recyclerView.addItemDecoration(DividerItemDecoration(context!!))
-        progress_bar.visibility = View.GONE
+        progress_bar?.visibility = View.GONE
     }
 }
